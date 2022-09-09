dde3a81be61d24c02ac8baae7198b5814aa97ff1
n3bojs4 <n3bojs4@gmail.com>
2022-09-09 08:57:07 +0000

    one line command to get ip address in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export FLASK_APP="webserver:create_app('dev')"
 FLASK_ENV=development
-flask run -p 5001 -h 127.0.0.1
+flask run -p 5001 -h $(ip -j addr show dev ens2 | awk -F '"local":"' '{print $2}' | awk -F '"' '{print $1}')
 
